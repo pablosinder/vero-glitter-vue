ada1624de336a738837afd81acc1699adf8a66b9
pablosinder <74592657+pablosinder@users.noreply.github.com>
2025-10-22 07:31:06 +0000

    first commit

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -2,5 +2,30 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as VueRouter from 'vue-router'
+import ProductsPage from './pages/ProductsPage.vue'
+import ProductDetailPage from './pages/ProductDetailPage.vue'
+import ShoppingCartPage from './pages/ShoppingCartPage.vue'
+import NotFoundPage from './pages/NotFoundPage.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(VueRouter.createRouter({
+    history: VueRouter.createMemoryHistory(),
+    routes: [{
+        path: '/cart',
+        component: ShoppingCartPage,
+    }, {
+        path: '/products',
+        component: ProductsPage,
+    }, {
+        path: '/products/:productId',
+        component: ProductDetailPage,
+    }, {
+        path: '/:pathMatch(.*)*',
+        component: NotFoundPage,
+    }, {
+        path: '/',
+        redirect: '/products'
+    }]
+}))
+.mount('#app')
